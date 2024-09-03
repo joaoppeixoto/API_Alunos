678fb37450016ff6503c5a50f89c03007e925fde
joao.peixoto678@gmail.com <joao.peixoto678@gmail.com>
2024-09-03 11:59:57 -0300

    Testando4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const studentRoutes = require('./routes/studentRoutes');
-const studentController = require('../controllers/studentController.js');
 
 const app = express();
 const port = 3000;
