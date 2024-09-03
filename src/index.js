04e7c89b2bbcda0f08cbd99397defb276206c8fd
joao.peixoto678@gmail.com <joao.peixoto678@gmail.com>
2024-09-03 12:05:49 -0300

    Add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const studentRoutes = require('./routes/studentRoutes');
